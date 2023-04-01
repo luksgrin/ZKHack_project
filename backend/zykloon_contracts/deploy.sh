0531a22fc80c36b17609213e9245548447f709b9
luksgrin <lugoibel@doctor.upv.es>
2023-04-01 18:16:23 +0200

    Deployment stuff but better

--- backend/zykloon_contracts/deploy.sh
+++ backend/zykloon_contracts/deploy.sh
@@ -1,2 +1,3 @@
 source .env
-forge script script/NFT.s.sol:MyScript --rpc-url $MUMBAI_RPC_URL --broadcast --verify -vvvv
+
+forge script "script/DeployZyKloon.s.sol:DeployZykloon" --rpc-url $MUMBAI_RPC_URL --broadcast --verify --etherscan-api-key $MUMBAI_POLYGONSCAN_API -vvvv
