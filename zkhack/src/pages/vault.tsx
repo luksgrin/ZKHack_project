06b6b0c3ba243484580d927b6c4fe189b946f2c9
pm <misirov@pablos-MacBook-Air.local>
2023-04-02 10:03:19 +0200

    sismo logo

--- zkhack/src/pages/vault.tsx
+++ zkhack/src/pages/vault.tsx
@@ -7,8 +7,11 @@ import { SismoButton } from "../components/SismoButton";
 
 import { ValidationComponent } from "../components/ValidationComponent";
 import React from "react";
-
+import Image from 'next/image'
 import Link from "next/link";
+import logo from '../public/logo.png';
+
+
 
 function Page() {
   const { isConnected } = useAccount();
@@ -43,13 +46,17 @@ function Page() {
                 fill-rule="evenodd"
                 clip-rule="evenodd"
               >
+                
                 <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z" />
               </svg>
             </div>
             <div className="Sismo_Text">Create Sismo Vault</div>
+            <Image src={logo} alt="Second Image" width={60} height={60} />
           </Link>
         </div>
-        <div className="ZyCloone_Container">{SismoBox()}</div>
+        <div className="ZyCloone_Container">
+          {SismoBox()}
+        </div>
       </div>
 
       {/* <button
